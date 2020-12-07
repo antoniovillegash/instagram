136797a4fc048a9065cb08b62ca373b303218c90
tony <antonio.villegas.0101@gmail.com>
2020-12-07 15:37:48 -0600

    se agregó el modelo de usuarios en posts

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,11 +1,15 @@
 """Platzigram URLs module """
+#django
+from django.contrib import admin
 from django.urls import path
+
 from platzigram.views import views as local_views
 from posts import views as posts_views
 
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('hello-world/',local_views.hello_world),
     path('sorted/',local_views.sort_integers),
     path('hi/<str:name>/<int:age>',local_views.say_hi),
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
+"""posts models"""
+#django
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """user model"""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
