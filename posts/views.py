acbc3491121b63642c8b47078b3ac892b2b11081
tony <antonio.villegas.0101@gmail.com>
2020-12-29 12:38:20 -0600

    posts view added

--- posts/views.py
+++ posts/views.py
@@ -9,12 +9,14 @@ posts = [
         'name': 'hola',
         'user': 'Erika muñoz',
         'timestamp': datetime.now().strftime('%b/%dth, %Y - %H:%M hrs'),
+        'photo': 'https://www.laughtard.com/wp-content/uploads/2019/02/50-Funny-Animal-Pictures-That-You-Need-In-Your-Life-37.jpg',
         'picture': 'https://picsum.photos/200/200/?image=1036',
     },
     {
         'name': 'hola',
         'user': 'Tony Villegas',
         'timestamp': datetime.now().strftime('%b/%dth, %Y - %H:%M hrs'),
+        'photo': 'https://www.laughtard.com/wp-content/uploads/2019/02/50-Funny-Animal-Pictures-That-You-Need-In-Your-Life-37.jpg',
         'picture': 'https://picsum.photos/200/200/?image=903',
     }
 ]
@@ -24,4 +26,4 @@ def list_posts(request):
     """listing existing posts"""
 
     
-    return render(request, 'feed.html', {'posts':posts})
+    return render(request, 'posts/feed.html', {'posts':posts})
